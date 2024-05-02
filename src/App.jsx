1a07b3982e627b8dd8e97a616a24c74cd15ccc21
Alexey Rudoy <leha1st@ya.ru>
2024-05-02 19:19:15 +0300

    feat: add Loader in PizzaBlock

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import pizzas from './assets/pizzas.json';
+import React from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
+import Loader from './components/PizzaBlock/Loader';
 import Sort from './components/Sort';
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://6633a521f7d50bbd9b4a2e13.mockapi.io/items')
+      .then((res) => res.json())
+      .then((array) => {
+        setItems(array);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,9 +30,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj, i) => (
-              <PizzaBlock key={i} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, i) => <Loader key={i} />)
+              : items.map((obj, i) => <PizzaBlock key={i} {...obj} />)}
           </div>
         </div>
       </div>
